9bc8dfbbfb620b9912fcb6f3d9dd28c8b23f2304
Samuel G. Mwangi <hirewebappdeveloper@gmail.com>
2023-02-24 09:12:20 +0000

    challenges solved

--- myApp.js
+++ myApp.js
@@ -1,5 +1,55 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+require('dotenv').config();
+
+app.use((req,res,next)=>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/", (req, res)=>{
+    const absolute_path = __dirname + "/views/index.html";
+    res.sendFile(absolute_path)
+})
+
+
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": 'Hello json'.toUpperCase()});
+      } else {
+        res.json({"message": 'Hello json'});
+      }
+});
+
+app.get("/now", (req, res, next)=>{
+  req.time = new Date().toString()
+  next()
+ }, (req, res)=>{
+    res.json({time: req.time})
+ })
+
+ app.get("/:word/echo", (req, res)=>{
+  const word = req.params.word;
+  res.json({echo: word})
+  console.log(word)
+})
+
+app.get("/name", (req,res)=>{
+  const first = req.query.first;
+  const last = req.query.last;
+  res.json({ name: `${first} ${last}`})
+})
+
+app.post("/name", (req, res)=>{
+  const first = req.body.first;
+  const last = req.body.last;
+  res.json({ name: `${first} ${last}`})
+})
 
 
 
@@ -31,9 +81,4 @@ let app = express();
 
 
 
-
-
-
-
-
- module.exports = app;
+module.exports = app;
